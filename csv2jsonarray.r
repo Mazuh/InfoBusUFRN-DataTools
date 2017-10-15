2d3e705e517ee75d4d51883fe6dc388f0b23afc0
Mazuh <marcell-mz@hotmail.com>
2017-10-15 08:40:28 -0300

    R-Studio hello world

---
+++ csv2jsonarray.r
@@ -0,0 +1,14 @@
+# Converts those csv into json arrays, also concatenating hours with minutes.
+
+filenames <- c(
+  "./schedules_datasets/direto_ru.csv",
+  "./schedules_datasets/expresso_ru_cet_vd.csv",
+  "./schedules_datasets/expresso_ru_reitoria_vd.csv",
+  "./schedules_datasets/expresso_vd_cet_ru.csv",
+  "./schedules_datasets/expresso_vd_cet.csv",
+  "./schedules_datasets/expresso_vd_reitoria_ru.csv",
+  "./schedules_datasets/expresso_vd_reitoria.csv",
+  "./schedules_datasets/inverso_ru.csv"
+)
+
+
