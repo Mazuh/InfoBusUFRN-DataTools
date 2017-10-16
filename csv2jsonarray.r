0c78d3889fe6dd014c48650ca0ffb0a61490d431
Mazuh <marcell-mz@hotmail.com>
2017-10-15 23:28:51 -0300

    Implement translation for a file

--- csv2jsonarray.r
+++ csv2jsonarray.r
@@ -9,6 +9,39 @@ filenames <- c(
   "./schedules_datasets/expresso_vd_reitoria_ru.csv",
   "./schedules_datasets/expresso_vd_reitoria.csv",
   "./schedules_datasets/inverso_ru.csv"
-)
+);
 
+schedulings <- read.csv(filenames[1]);
 
+identation <- paste(replicate(7, "  "), collapse="");
+
+for(i in 1:nrow(schedulings)) {
+  hour <- schedulings[i, 1];
+  
+  if (as.numeric(hour) < 10){
+    hour <- paste("0", hour, sep="");
+  }
+  
+  for (min_op in schedulings[i, 2:ncol(schedulings)]){
+    if (!is.na(min_op)){
+      
+      if (as.numeric(min_op) < 10){
+        min_op <- paste("0", min_op, sep="");
+      }
+      
+      print(paste(
+        identation,
+        "{\"time\": \"",
+        hour, ":", min_op,
+        "\"},",
+        sep=""
+      ));
+      
+      }
+  }
+}
+
+for (filename in filenames){
+  schedulings = read.csv(filename);
+  paste(paste(replicate(2, "  "), collapse=""), "ok", sep="");
+}
